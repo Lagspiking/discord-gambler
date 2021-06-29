a70a32e40a41ff0c99672097061be80375873efd
Sparks <13735241+Childish@users.noreply.github.com>
2021-06-29 13:53:45 +0100

    Aliases

--- commands/coins.py
+++ commands/coins.py
@@ -4,7 +4,7 @@ class CoinsCommand(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
     
-    @commands.command(name="coins")
+    @commands.command(name="coins", aliases="coin")
     async def on_coins_command(self, ctx):
         economy = self.bot.get_cog("Economy")
         await ctx.send(f"You have {await economy.get_wallet(ctx.author)} coins available.")
