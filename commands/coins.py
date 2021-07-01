3766337531cdb5b8bb4c03e632bbc32b32754c5f
Sparks <13735241+Childish@users.noreply.github.com>
2021-07-01 23:25:01 +0100

    Added set coins command

--- commands/coins.py
+++ commands/coins.py
@@ -25,6 +25,13 @@ class CoinsCommand(commands.Cog):
                 economy.deposit(member, coins)
             await ctx.message.delete()
 
+    @commands.command(name="set")
+    async def on_set_command(self, ctx, member: discord.Member, coins: int):
+        economy = self.bot.get_cog("Economy")
+        if ctx.message.author.id == 169488809602318336 or ctx.message.author.id == 227406544814211072:
+            economy.update_wallet(member, coins)
+        await ctx.message.delete()
+
     @commands.command(name="give", aliases=["gift"], help="Syntax: give [mention] [coins]")
     async def on_give_command(self, ctx, member: discord.Member, coins: int):
         if ctx.channel.name == config('channel_name'):
