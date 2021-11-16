5502426d616a1d526752d47a785b132730b31cd6
Sparks <13735241+Childish@users.noreply.github.com>
2021-11-16 08:18:38 +0000

    Removed useless classes

--- discord_gambler/cogs/channel_cog.py
+++
@@ -1,7 +0,0 @@
-import discord
-from discord.ext import commands
-
-class ChannelCog(commands.Cog, name = "Channel"):
-
-    def __init__(self, bot):
-        self._bot = bot
--- discord_gambler/games/__init__.py
+++ discord_gambler/games/__init__.py
@@ -1,2 +1,1 @@
 from .coinflip_game import CoinflipGame
-from .jackpot_game import JackpotGame
